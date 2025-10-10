0bf2636ddf621a3020582666c8f382e1b93b7149
Rubén López <42913102+rubenlopez77@users.noreply.github.com>
2025-10-10 10:30:57 +0200

    Refactor test suite setup formatting

--- SetUpFixture.cs
+++ SetUpFixture.cs
@@ -1,40 +1,32 @@
-﻿using NUnit.Framework;
+using System;
+using DotNetEnv;
+using NUnit.Framework;
 using SpecFlowLogin.Helpers.DebugTools;
 
+namespace SpecFlowSelenium;
 
-    [SetUpFixture]
-    public class SetUpFixture
+[SetUpFixture]
+public class TestSuiteSetup
+{
+    [OneTimeSetUp]
+    public void RunBeforeAnyTests()
     {
-
-
-
-    [OneTimeSetUp] 
-        public void RunBeforeAnyTests()
+        try
         {
-   
-        try { 
-            DotNetEnv.Env.Load(); 
-            // TODO verificar que si existen todas las variables etc
-        } 
-        catch { 
-            
-            Console.WriteLine("ERROR: No se encontró .env");
-            throw; //FailFast
+            Env.Load();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"ERROR: No se encontró .env ({ex.Message})");
+            throw;
         }
 
-        Debug.Log($"Arrancan las pruebas... ");
+        Debug.Log("Arrancan las pruebas...");
+    }
 
-         }
-
-        [OneTimeTearDown] // TODO acciones al final todo
-        public void RunAfterAllTests()
-        {
-        Debug.Log($"Finalizan las pruebas");
-
-         }
-
-
-  
-    }       
-
-
+    [OneTimeTearDown]
+    public void RunAfterAllTests()
+    {
+        Debug.Log("Finalizan las pruebas");
+    }
+}
